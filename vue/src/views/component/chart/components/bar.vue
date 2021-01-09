eabe5f450c34ad452f02896b8915ac2ec431f87b
qyhever <15927700473@1631.com>
2021-01-09 20:12:37 +0800

    :art: update views/component/chart

--- vue/src/views/component/chart/components/bar.vue
+++ vue/src/views/component/chart/components/bar.vue
@@ -1,74 +1,39 @@
 <template>
-  <div
-    ref="container"
-    class="p10"
-  ></div>
+  <div class="chart" ref="chartRef"></div>
 </template>
 
 <script>
-import { Chart } from '@antv/g2'
-export default {
-  mounted() {
-    const data = [
-      { type: '西北', value: 82 },
-      { type: '西南', value: 130 },
-      { type: '华北', value: 245 },
-      { type: '东北', value: 268 },
-      { type: '中南', value: 414 },
-      { type: '华东', value: 458 }
-    ]
-    const chart = new Chart({
-      container: this.$refs.container,
-      autoFit: true,
-      height: 400,
-      appendPadding: [20, 20, 20, 20]
-    })
-    chart.data(data)
-    chart.scale({
-      value: {
-        alias: '销量额'
-      }
-    })
-    chart.axis('type', {
-      title: null,
-      tickLine: null,
-      line: null,
-      subTickLine: null
+import { defineComponent, ref, onMounted } from 'vue'
+import { getOption } from './bar-option'
+import useChartResize from '@/hooks/useChartResize'
+
+export default defineComponent({
+  setup() {
+    const chartRef = ref(null)
+    const chartIns = ref(null)
+    useChartResize(chartIns)
+
+    onMounted(() => {
+      init() // eslint-disable-line
     })
 
-    chart.axis('value', {
-      label: null,
-      tickLine: null,
-      subTickLine: null,
-      line: null,
-      title: {
-        offset: 30,
-        style: {
-          fontSize: 12,
-          fontWeight: 300
-        }
+    function init() {
+      if (!chartRef.value) {
+        return
       }
-    })
-    chart.legend(false)
-    chart.tooltip({
-      showMarkers: false
-    })
-    chart.coordinate().transpose()
-    chart
-      .interval()
-      .position('type*value')
-      .size(26)
-      .label('value', v => {
-        return {
-          style: {
-            fill: '#fff'
-          },
-          position: 'middle',
-          content: v + '万'
-        }
-      })
-    chart.interaction('active-region')
-    chart.render()
+      const chart = chartIns.value = window.echarts.init(chartRef.value)
+      chart.setOption(getOption())
+    }
+
+    return {
+      chartRef
+    }
   }
-}
+})
 </script>
+
+<style lang="less" scoped>
+  .chart {
+    height: 400px;
+  }
+</style>
--- vue/src/views/component/chart/components/column.vue
+++ vue/src/views/component/chart/components/column.vue
@@ -1,93 +1,39 @@
 <template>
-  <div ref="container" class="p10"></div>
+  <div class="chart" ref="chartRef"></div>
 </template>
 
 <script>
-import { Chart } from '@antv/g2'
-export default {
-  mounted() {
-    const data = [
-      {
-        type: '家具家电',
-        sales: 38
-      },
-      {
-        type: '粮油副食',
-        sales: 52
-      },
-      {
-        type: '生鲜水果',
-        sales: 61
-      },
-      {
-        type: '美容洗护',
-        sales: 145
-      },
-      {
-        type: '母婴用品',
-        sales: 48
-      },
-      {
-        type: '进口食品',
-        sales: 38
-      },
-      {
-        type: '食品饮料',
-        sales: 38
-      },
-      {
-        type: '家庭清洁',
-        sales: 38
+import { defineComponent, ref, onMounted } from 'vue'
+import { getOption } from './column-option'
+import useChartResize from '@/hooks/useChartResize'
+
+export default defineComponent({
+  setup() {
+    const chartRef = ref(null)
+    const chartIns = ref(null)
+    useChartResize(chartIns)
+
+    onMounted(() => {
+      init() // eslint-disable-line
+    })
+
+    function init() {
+      if (!chartRef.value) {
+        return
       }
-    ]
-    const chart = new Chart({
-      container: this.$refs.container,
-      autoFit: true,
-      height: 400,
-      appendPadding: [20, 20, 20, 20]
-    })
+      const chart = chartIns.value = window.echarts.init(chartRef.value)
+      chart.setOption(getOption())
+    }
 
-    chart.data(data)
-    chart.scale('sales', {
-      nice: true,
-      alias: '销售量'
-    })
-    chart.scale('type', {
-      alias: '类别'
-    })
-
-    chart.axis('sales', {
-      title: {
-        offset: 50,
-        style: {
-          opacity: 0.8
-        }
-      }
-    })
-
-    chart.axis('type', {
-      title: {
-        offset: 50,
-        style: {
-          opacity: 0.8
-        }
-      },
-      line: {
-        style: {
-          color: '#000000'
-        }
-      },
-      tickLine: null
-    })
-
-    chart.tooltip({
-      showMarkers: true
-    })
-    chart.interaction('active-region')
-
-    chart.interval().position('type*sales')
-
-    chart.render()
+    return {
+      chartRef
+    }
   }
-}
+})
 </script>
+
+<style lang="less" scoped>
+  .chart {
+    height: 400px;
+  }
+</style>
