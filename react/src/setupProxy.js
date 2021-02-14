b50441886a81862adfdb7e0429c68e88ba175fa2
lgf <18827634408@163.com>
2021-02-14 15:29:09 +0800

    :muscle: development version 2.0

--- react/src/setupProxy.js
+++ react/src/setupProxy.js
@@ -3,12 +3,12 @@
  */
 const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
-    app.use(createProxyMiddleware('/api', 
-        { 
-            // target: 'http://localhost:8080',
-            target: 'http://115.29.224.69:8080',
+    app.use(createProxyMiddleware('/api',
+        {
+            target: 'http://localhost:8080',
+            // target: 'http://115.29.224.69:8080',
             changeOrigin: true,
-            pathRewrite: {'^/api': '/api'} 
+            pathRewrite: {'^/api': '/api'}
         }
     ));
 }
