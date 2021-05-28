70bca96715ba8ba4e8d044657cc80727a6fcc72f
lgf <18827634408@163.com>
2021-05-28 09:52:29 +0800

    organize typesetting

--- react+typescript/src/components/svgIcon/index.tsx
+++ react+typescript/src/components/svgIcon/index.tsx
@@ -1,20 +1,35 @@
-import React, { memo, useMemo } from 'react'
+import React, { memo, useMemo } from 'react';
 import Icon from '@ant-design/icons';
 export type svgProps = {
-  iconClass: string,
-  fill?: string,
-  fontSize?: string,
-  className?: string
-}
+  iconClass: string;
+  fill?: string;
+  fontSize?: string;
+  className?: string;
+};
 
-const SvgIcon: React.FC<svgProps> = memo(function SvgIcon({ iconClass, fill, fontSize, className, ...props }) {
-  const iconName = useMemo(() => ("#icon-" + iconClass), [iconClass]);
+const SvgIcon: React.FC<svgProps> = memo(function SvgIcon({
+  iconClass,
+  fill,
+  fontSize,
+  className,
+  ...props
+}) {
+  const iconName = useMemo(() => '#icon-' + iconClass, [iconClass]);
   return (
-    <Icon {...props} component={() => (<svg style={{ ...svgStyle, fontSize }} aria-hidden="true" className={className}>
-      <use xlinkHref={iconName} fill={fill} /></svg>)}>
-    </Icon>
-  )
-})
+    <Icon
+      {...props}
+      component={() => (
+        <svg
+          style={{ ...svgStyle, fontSize }}
+          aria-hidden="true"
+          className={className}
+        >
+          <use xlinkHref={iconName} fill={fill} />
+        </svg>
+      )}
+    ></Icon>
+  );
+});
 
 const svgStyle = {
   width: '1em',
@@ -22,7 +37,7 @@ const svgStyle = {
   verticalAlign: '-0.15em',
   overflow: 'hidden',
   fill: 'currentColor', // 颜色值
-  fontSize: '1.1em'
-}
+  fontSize: '1.1em',
+};
 
-export default SvgIcon
+export default SvgIcon;
--- react/src/index.js
+++ react/src/index.js
@@ -1,26 +1,25 @@
-import 'core-js'
-import 'react-app-polyfill/ie11'
-import 'react-app-polyfill/stable'
+import 'core-js';
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Routers from '@/router'
+import Routers from '@/router';
 import { ConfigProvider } from 'antd';
 import 'moment/locale/zh-cn';
 import locale from 'antd/es/locale/zh_CN';
-import { Provider } from 'react-redux'
-import store from '@/redux/store'
+import { Provider } from 'react-redux';
+import store from '@/redux/store';
 import '@/icons'; // 初始化svg加载
 import './index.css';
-import './assets/scss/common.scss'
-import './assets/scss/update.antd.css'
-import './assets/scss/animate.scss'
+import './assets/scss/common.scss';
+import './assets/scss/update.antd.css';
+import './assets/scss/animate.scss';
 
 ReactDOM.render(
   <ConfigProvider locale={locale}>
     <Provider store={store}>
       <Routers />
     </Provider>
- </ConfigProvider>,
-  document.getElementById('root')
+  </ConfigProvider>,
+  document.getElementById('root'),
 );
-
