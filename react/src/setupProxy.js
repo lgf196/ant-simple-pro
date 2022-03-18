8361fb041a9aca764f90cfd06877120b18dbbbdd
lgf196 <2219014328@qq.com>
2022-03-18 10:36:41 +0800

    Update setupProxy.js

--- react/src/setupProxy.js
+++ react/src/setupProxy.js
@@ -6,7 +6,6 @@ module.exports = function(app) {
     app.use(createProxyMiddleware('/api',
         {
             target: 'http://localhost:8080',
-            // target: 'http://115.29.224.69:8080',
             changeOrigin: true,
             pathRewrite: {'^/api': '/api'}
         }
